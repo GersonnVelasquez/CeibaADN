28c8cb8128cf1a93b89057aa484ec79bed4bb5ff
Gersonn Velasquez <yersonn.velasquez@gmail.com>
2021-07-24 16:13:31 -0600

    Carga inicial

---
+++ src/main/java/com/adn/ceiba/CeibaApplication.java
@@ -0,0 +1,13 @@
+package com.adn.ceiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeibaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adn/ceiba/CeibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adn.ceiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
